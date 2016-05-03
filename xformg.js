f03696b75719342ee97b45879a4dfcc3c63c4513
l.witte <leonard.witte@campus.tu-berlin.de>
2016-05-03 13:50:35 +0200

    finished xformg.js

--- xformg.js
+++ xformg.js
@@ -1,31 +1,23 @@
 function xforml(o1, o2, isServer) {
 	if (o1["o"] == "i") {
 		if (o2["o"] == "i") {
-			if (o1["k"] < o2["k"])  { o2["k"]++; return [o1, o2] }
-			if (o1["k"] > o2["k"])  { o1["k"]++; return [o1, o2] }
-			if (o1["k"] == o2["k"]) { o2["k"]++; return [o1, o2] }
+			return xformgii(op1,op2,isServer);
 		}
 		if (o2["o"] = "d") {
-			if (o1["k"] < o2["k"])  { o2["k"]++; return [o1, o2] }
-			if (o1["k"] > o2["k"])  { o2["k"]--; return [o1, o2] }
-			if (o1["k"] < o2["k"])  { o2["k"]++; return [o1, o2] }
+			return xformgid(op1,op2);
 		}
 	}
 	if (o2["o"] == "d") {
 		if (o2["o"] == "i") {
-			if (o1["k"] < o2["k"])  { o2["k"]--; return [o1, o2] }
-			if (o1["k"] > o2["k"])  { o1["k"]++; return [o1, o2] }
-			if (o1["k"] == o2["k"]) { o1["k"]++; return [o1, o2] }
+			return xformgid(op2,op1);
 		}
 		if (o2["o"] = "d") {
-			if (o1["k"] < o2["k"])  { o2["k"]--; return [o1, o2] }
-			if (o1["k"] > o2["k"])  { o1["k"]--; return [o1, o2] }
-			if (o1["k"] < o2["k"])  { return [{"o":"n"}, {"o":"n"}]}
+			return xformgdd(op1,op2);
 		}
 	}
 }
 
-function xformgii(op1, op2) {
+function xformgii(op1, op2, isServer) {
 	if (op1.k.length > op2.k.length) {
 		if (op2.k.isSublistOf(op2.k)) {
 			if ( op1.k[op2.k.length] >= op2.i ) {
@@ -124,7 +116,48 @@ function xformgdd(op1,op2) {
 			if (op1.k[op2.k.length] == op2.i) {
 				op1.o = 'n';
 				return [op1,op2];
-					  }
+			}
+		} else {
+			return [op1,op2];
+		}
+	} else if (op1.k.length < op2.k.length) {
+		if (op1.k.isSublistOf(op2.k)) {
+			if (op2.k[op2.k.length] > op1.i) {
+				op2.k[op1.k.length]--;
+				return [op1,op2];
+			}
+			if (op2.k[op1.k.length] == op1.i) {
+				op2.o = 'n';
+				return [op1,op2];
+			}
+		} else {
+			return [op1,op2];
+		}
+	} else if (op1.k.length == op2.k.length) {
+		if (op1.k.isSublistOf(op2.k)) {
+			if (op1.i < op2.i) {
+				op2.i--;
+				return [op1,op2];
+			}
+			if (op1.i > op2.i) {
+				op1.i--;
+				return [op1,op2];
+			}
+			if (op1.i == op2.i) {
+				op1.o = 'n';
+				op2.o = 'n';
+				return [op1,op2];
+			}
+		} else {
+			return [op1,op2];
+		}
+	}
+}
+
+function CONFLICT_HANDLER(op1,op2) {
+	op1.o = 'n';
+	return [op1,op2];
+}
 
 function isSublistOf(k) {
 	for (var i = 0; i < this.length; i++) {
